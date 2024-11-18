5a0f8c78f0dc69d6457750f8a4f8f33e6252b891
alexandrueneten <50365187+alexandrueneten@users.noreply.github.com>
2024-11-18 10:23:32 +0200

    Exclude any OPTIONS request from authentication (#11)
    Co-authored-by: Guillem Barba <GuillemBarbaDomingo@tengroup.com>

--- aiohttp_jwt/utils.py
+++ aiohttp_jwt/utils.py
@@ -1,8 +1,12 @@
 import asyncio
 import re
 
+from aiohttp.hdrs import METH_OPTIONS
+
 
 def check_request(request, entries):
+    if request.method == METH_OPTIONS:
+        return True
     for pattern in entries:
         if re.match(pattern, request.path):
             return True
